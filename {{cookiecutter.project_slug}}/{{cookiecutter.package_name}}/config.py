2fd807a9ac47e26b4022c5646dc11d49378cfdd3
Alan Williams <theydonthaveit@gmail.com>
2018-02-25 16:54:45 +0000

    ready for postgres use

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/config.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/config.py
@@ -11,7 +11,10 @@ class Config(object):
 class Dev(Config):
 
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////opt/code/{{cookiecutter.package_name}}.db'
+    if {{cookiecutter.database_used}} == 'sqlite':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////opt/code/{{cookiecutter.package_name}}.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgresql://{{ cookiecutter.postgres_host }}:{{ cookiecutter.postgres_port }}/{{ cookiecutter.postgres_db }}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BASIC_AUTH_USERNAME = 'admin'
     BASIC_AUTH_PASSWORD = 'secret'
